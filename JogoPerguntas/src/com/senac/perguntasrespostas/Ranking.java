dcc7515bbdfc185efbd44418b68565ac964f277a
unknown <vinicius.araujo8228@gmail.com>
2022-11-18 14:00:45 -0300

    Arrumando Ranking 3

---
+++ JogoPerguntas/src/com/senac/perguntasrespostas/Ranking.java
@@ -0,0 +1,23 @@
+package com.senac.perguntasrespostas;
+//Classe Ranking
+
+public class Ranking {
+	private String nome;
+    private int pontos;
+    
+    public Ranking() {}
+    
+    public String getNome() {
+	return nome;
+    }
+    public void setNome(String nome) {
+	this.nome = nome;
+    }
+    
+    public int getPontos() {
+	return pontos;
+    }
+    public void setPontos(int pontos) {
+	this.pontos = pontos;
+    }
+}
