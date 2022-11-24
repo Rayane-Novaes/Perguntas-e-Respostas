23c44eef347eca6fe943b8707ee78ac0b8b696ff
Pamera-png <pameranatali@gmail.com>
2022-11-24 12:25:28 -0300

    recuperação codigo regras

--- JogoPerguntas/src/com/senac/perguntasrespostas/Regras.java
+++ JogoPerguntas/src/com/senac/perguntasrespostas/Regras.java
@@ -1,26 +1,26 @@
 package com.senac.perguntasrespostas;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class Regras {
 	public static void instrucoes() throws Exception {
-		String arquivo = "Regras/Instrucoes.txt";
-		Path caminho = Paths.get(arquivo);
+		FileInputStream abrirArquivo = new FileInputStream("Regras/Instrucoes.txt");
+		  InputStreamReader entradaFormatada = new InputStreamReader(abrirArquivo);
+		  		  BufferedReader arquivoRegras = new BufferedReader(entradaFormatada);		     
+		  String linha;
+		  		  while ((linha = arquivoRegras.readLine()) != null) {
+		   System.out.println(linha);
+		   		  } // do laco
+		  arquivoRegras.close();
 
-		try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(Files.newInputStream(caminho)))) {
-			for (String line : bufferedReader.lines().toList())
-				System.out.println(line);
-		}
-		System.out.println("Deseja voltar para o menu inicial?");
+				System.out.println("Deseja voltar para o menu inicial?");
         System.out.println("[1]Sim [2]Não");
         int input = Base.leiaInput(" > ", 2);
         if (input == 1){
             Base.menu();
-        }
+        } //do if
 		
-	}
+	} //da funcao
 }
